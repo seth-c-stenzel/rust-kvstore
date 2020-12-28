b0fcb90270af36d1b06f6bf3ad64895ef5557fe3
seth-c-stenzel <seth.c.stenzel@gmail.com>
2020-12-28 17:21:49 -0600

    Added DB Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::collections::HashMap;
+use std::io::Error as ioError;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+    let key = args.next().unwrap();
+    let value = args.next().unwrap();
+
+    let db = Database::new();
+
+    let  contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents);
+    println!("Key: {} | Value: {} pair has been stored", key, value);
+}
+
+struct Database {
+    inner: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, ioError> {
+        
+        /* Using the ? operator allows us ot use this pattern
+        let contents = match std::fs::read_to_string("kv.db") {
+            Result::Ok(contents) => contents,
+            Result::Err(e) => return Err(e),
+        };
+        */
+        let contents = std::fs::read_to_string("kv.db")?;
+
+        Ok(Database {
+            inner: HashMap::new()
+        })
+    }
 }
